8cddefb71eed93ee04ef8a61bdbe5b9da93d608f
PravasGiri7 <gpravas4@gmail.com>
2024-11-03 18:42:51 +0900

    fixed the UI error

--- frontend/lib/src/screens/profile_screen.dart
+++ frontend/lib/src/screens/profile_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:jim/src/constants/sizes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jim/src/screens/profile_menu.dart';
@@ -8,53 +7,72 @@ import 'package:jim/src/screens/update_profile.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import '../constants/image_strings.dart';
 
-
-class ProfileScreen extends StatelessWidget{
-  const ProfileScreen({Key? key}) : super(key:key);
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white, // Set the background color of the Scaffold to white
         body: SingleChildScrollView(
-          child:Container(
+          child: Container(
             padding: const EdgeInsets.all(tDefaultSize),
+            color: Colors.white, // Optional: Set the container's background color to white
             child: Column(
               children: [
                 Stack(
                   children: [
                     SizedBox(
-                      width: 120, height: 120,
-                      child: ClipRRect(borderRadius:BorderRadius.circular(100),child: Image(image: AssetImage(WelcomeScreenImage),)),
+                      width: 120,
+                      height: 120,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image(image: AssetImage(WelcomeScreenImage)),
+                      ),
                     ),
                     Positioned(
                       bottom: 0,
                       right: 0,
                       child: Container(
-                          width: 35,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(100),
-                            color: Colors.yellow,
-                          ),
-                          child: const Icon(
-                              LineAwesomeIcons.pencil_alt_solid, size: 20.0, color: Colors.black)
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.yellow,
+                        ),
+                        child: const Icon(
+                          LineAwesomeIcons.pencil_alt_solid,
+                          size: 20.0,
+                          color: Colors.black,
+                        ),
                       ),
                     )
-                  ]
+                  ],
                 ),
                 const SizedBox(height: 10),
-                Text('xyzname', style: GoogleFonts.anton(fontSize: 20),),
-                Text('useremail@gmail.com', style: GoogleFonts.anton(fontSize: 20),),
+                Text(
+                  'xyzname',
+                  style: GoogleFonts.anton(fontSize: 20),
+                ),
+                Text(
+                  'useremail@gmail.com',
+                  style: GoogleFonts.anton(fontSize: 20),
+                ),
                 const SizedBox(height: 20),
-                SizedBox(width:200,
+                SizedBox(
+                  width: 200,
                   child: ElevatedButton(
-                      onPressed: () => Get.to(()=> const UpdateProfileScreen()),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey, side: BorderSide.none, shape: const StadiumBorder()),
-                      child: Text("Edit Profile", style: TextStyle(color: Colors.black, fontSize: 20))),
+                    onPressed: () => Get.to(() => const UpdateProfileScreen()),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      side: BorderSide.none,
+                      shape: const StadiumBorder(),
+                    ),
+                    child: Text("Edit Profile",
+                        style: TextStyle(color: Colors.black, fontSize: 20)),
+                  ),
                 ),
                 const SizedBox(height: 30),
                 const Divider(),
@@ -87,14 +105,12 @@ class ProfileScreen extends StatelessWidget{
                   textColor: Colors.red,
                   endIcon: false,
                   onPress: () {},
-                ),
-      
+                )
               ],
             ),
-          )
+          ),
         ),
       ),
     );
   }
 }
-
