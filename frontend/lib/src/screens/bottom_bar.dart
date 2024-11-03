b5316efd8f4e27ece101cb6891e910d1e2f8ab70
PravasGiri7 <gpravas4@gmail.com>
2024-11-03 18:42:25 +0900

    fixed the design

--- frontend/lib/src/screens/bottom_bar.dart
+++ frontend/lib/src/screens/bottom_bar.dart
@@ -1,10 +1,10 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
+import 'package:get/get.dart';
+import 'package:jim/src/screens/try.dart';
 import 'home_page.dart';
 import 'previous_order.dart';
 import 'chat_ui.dart';
-import 'profile_screen.dart';
 import 'add_listing.dart';
 
 class BottomBar extends StatefulWidget {
@@ -14,79 +14,36 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBar extends State<BottomBar> {
-  int _bottomNavIndex = 0;
 
-  final List<IconData> homeicons = [
-    Icons.home,
-    Icons.document_scanner,
-    Icons.chat,
-    Icons.person,
-  ];
-
-  // Replace the IndexedStack and directly display the selected page
-  Widget _getSelectedPage(int index) {
-    switch (index) {
-      case 0:
-        return HomeScreen();
-      case 1:
-        return Previous_Order_Screen();
-      case 2:
-        return ChatScreen();
-      case 3:
-        return ProfileScreen();
-      default:
-        return HomeScreen();
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          ['Home', 'Previous', 'Chat', 'Profile',][_bottomNavIndex],
-          style: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-            fontSize: 24,
-          ),
+      bottomNavigationBar: Obx(
+        ()=> NavigationBar(
+
+          elevation: 0,
+            selectedIndex: controller.selectedindex.value,
+            onDestinationSelected: (index) => controller.selectedindex.value=index,
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+              NavigationDestination(icon: Icon(Icons.document_scanner), label: "Previous"),
+              NavigationDestination(icon: Icon(Icons.add), label: "Add Listing"),
+              NavigationDestination(icon: Icon(Icons.chat), label: "Chat Bot"),
+              NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+        ]
         ),
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        elevation: 0.0,
-        actions: [
-          const Icon(Icons.notifications, color: Colors.black, size: 30.0),
-        ],
       ),
-      body: _getSelectedPage(_bottomNavIndex),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            PageTransition(
-              child: const AddListingScreen(),
-              type: PageTransitionType.bottomToTop,
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.white,
-        shape: const CircleBorder(),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: AnimatedBottomNavigationBar(
-        splashColor: Colors.red,
-        activeColor: Colors.yellow,
-        inactiveColor: Colors.black.withOpacity(0.5),
-        icons: homeicons,
-        activeIndex: _bottomNavIndex,
-        gapLocation: GapLocation.center,
-        notchSmoothness: NotchSmoothness.softEdge,
-        onTap: (index) {
-          setState(() {
-            _bottomNavIndex = index;
-          });
-        },
-      ),
+      body: Obx(()=>controller.screens[controller.selectedindex.value]),
     );
   }
+}
+
+
+class NavigationController extends GetxController{
+  final Rx<int> selectedindex=0.obs;
+
+  final screens=[const HomeScreen(), const Previous_Order_Screen(), const AddListingScreen(), const ChatScreen(), const TryScreen()];
 }
