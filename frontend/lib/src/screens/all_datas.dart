4852f53b9dc56127fcbc1c74172a405d5946aa14
PravasGiri7 <gpravas4@gmail.com>
2024-11-04 21:36:55 +0900

    file that has jwtTokens

--- frontend/lib/src/screens/all_datas.dart
+++ frontend/lib/src/screens/all_datas.dart
@@ -4,3 +4,8 @@ class UserController extends GetxController {
   final String email;
   UserController({required this.email});
 }
+
+class UserController2 extends GetxController{
+  final String token;
+  UserController2({required this.token});
+}
