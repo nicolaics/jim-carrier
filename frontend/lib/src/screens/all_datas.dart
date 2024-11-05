16e88dd2e064613755bf466e0d1a7cd14e871792
PravasGiri7 <gpravas4@gmail.com>
2024-11-05 23:44:51 +0900

    changes for google-sign in

--- frontend/lib/src/screens/all_datas.dart
+++ frontend/lib/src/screens/all_datas.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class UserController extends GetxController {
   final String email;
@@ -8,4 +10,31 @@ class UserController extends GetxController {
 class UserController2 extends GetxController{
   final String token;
   UserController2({required this.token});
+}
+
+class Controller extends GetxController {
+
+  Future<User?> loginWithGoogle() async{
+    final googleAccount= await GoogleSignIn().signIn();
+
+    final googleAuth= await googleAccount?.authentication;
+
+    //signing in with firebase auth
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    final userCredential = await FirebaseAuth.instance.signInWithCredential(
+      credential,
+    );
+
+    return userCredential.user;
+  }
+
+  Future<void> signOut() async{
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+  }
+
 }
