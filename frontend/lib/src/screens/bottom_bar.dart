e6e2746ccb4d4fda064f97799f1f98b7ad144a5c
PravasGiri7 <gpravas4@gmail.com>
2024-11-05 23:44:57 +0900

    changes for google-sign in

--- frontend/lib/src/screens/bottom_bar.dart
+++ frontend/lib/src/screens/bottom_bar.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jim/src/screens/try.dart';
@@ -9,39 +8,46 @@ import 'add_listing.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
+
   @override
   _BottomBar createState() => _BottomBar();
 }
 
 class _BottomBar extends State<BottomBar> {
-@override
+  @override
   Widget build(BuildContext context) {
     final controller = Get.put(NavigationController());
 
     return Scaffold(
       bottomNavigationBar: Obx(
-        ()=> NavigationBar(
-
+            () => NavigationBar(
           elevation: 0,
-            selectedIndex: controller.selectedindex.value,
-            onDestinationSelected: (index) => controller.selectedindex.value=index,
-            destinations: [
-              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-              NavigationDestination(icon: Icon(Icons.document_scanner), label: "Previous"),
-              NavigationDestination(icon: Icon(Icons.add), label: "Add Listing"),
-              NavigationDestination(icon: Icon(Icons.chat), label: "Chat Bot"),
-              NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-        ]
+          selectedIndex: controller.selectedindex.value,
+          onDestinationSelected: (index) => controller.selectedindex.value = index,
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+            NavigationDestination(icon: Icon(Icons.document_scanner), label: "Previous"),
+            NavigationDestination(icon: Icon(Icons.add), label: "Add Listing"),
+            NavigationDestination(icon: Icon(Icons.chat), label: "Chat Bot"),
+            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+          ],
         ),
       ),
-      body: Obx(()=>controller.screens[controller.selectedindex.value]),
+      body: Obx(() => controller.screens[controller.selectedindex.value]),
     );
   }
 }
 
+class NavigationController extends GetxController {
+  // Set the initial index to 0 to show the HomeScreen first
+  final Rx<int> selectedindex = 0.obs;
 
-class NavigationController extends GetxController{
-  final Rx<int> selectedindex=0.obs;
-
-  final screens=[const HomeScreen(), const Previous_Order_Screen(), const AddListingScreen(), const ChatScreen(), const TryScreen()];
+  // Define the list of screens
+  final screens = [
+    const HomeScreen(),
+    const Previous_Order_Screen(),
+    const AddListingScreen(),
+    const ChatScreen(),
+    const TryScreen(),
+  ];
 }
