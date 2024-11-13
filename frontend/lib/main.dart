763c0bbc2fa6b4fbd5c7561915f71f10be6fbd25
PravasGiri7 <gpravas4@gmail.com>
2024-11-13 16:53:44 +0900

    for testing

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jim/firebase_options.dart';
 import 'package:jim/src/screens/welcome.dart';
+import 'package:jim/src/screens/home/home_page.dart';
+import 'package:jim/src/screens/order/previous_order.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
