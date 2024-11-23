f04b11bd2853d4d1aa900ed8ac4328058c3a0ec4
PravasGiri7 <gpravas4@gmail.com>
2024-11-23 19:57:10 +0900

    order page

--- frontend/lib/src/screens/order/new_order.dart
+++ frontend/lib/src/screens/order/new_order.dart
@@ -1,7 +1,9 @@
-import 'dart:convert';
+import 'package:jim/src/screens/home/bottom_bar.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';  // Import the intl package
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'dart:typed_data';
 import '../../api/api_service.dart';
 
@@ -17,8 +19,9 @@ class NewOrder extends StatefulWidget {
 }
 
 class _NewOrderState extends State<NewOrder> {
-  final TextEditingController _weightController = TextEditingController(); // Controller for the weight input
-
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _contentsController = TextEditingController();// Controller for the weight input
+  final ApiService apiService = ApiService();
   @override
   Widget build(BuildContext context) {
     final carrier = widget.carrier; // Access the passed carrier details
@@ -59,6 +62,7 @@ class _NewOrderState extends State<NewOrder> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+
                     _buildDetailRow(Icons.person, "Name:", carrier["name"] ?? "Unknown"),
                     _buildDetailRow(Icons.location_on, "Destination:", carrier["destination"] ?? "No destination"),
                     _buildDetailRow(Icons.attach_money, "Price:", carrier["price"] ?? "No price"),
@@ -96,12 +100,30 @@ class _NewOrderState extends State<NewOrder> {
               ),
             ),
             const SizedBox(height: 24),
+            const Text(
+              'Contents Info',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              controller: _contentsController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: const BorderSide(color: Colors.black),
+                ),
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 ElevatedButton(
                   onPressed: () {
                     final orderWeight = _weightController.text;
+
                     print("Pay Now for $orderWeight kg");
                     // Handle Pay Now logic here
                   },
@@ -118,10 +140,52 @@ class _NewOrderState extends State<NewOrder> {
                   ),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    final orderWeight = _weightController.text;
+                  onPressed: () async {
+                    final orderWeight = _weightController.text.trim();
+                    double weight = double.tryParse(orderWeight) ?? 0.0;
+                    print("weight $weight");
                     print("Pay Later for $orderWeight kg");
+                    print(carrier["price"]);
+                    print(carrier["id"]);
+                    print(carrier["currency"]);
+                    final regex = RegExp(r'^([^\d]+)?([\d,\.]+)');
+                    final match = regex.firstMatch(carrier["price"]);
+                    double carrierPrice = 0.0;
+                    String currencySymbol = "";
+
+
+                    if (match != null) {
+                      // Capture the currency symbol and numeric value
+                      currencySymbol = match.group(1)?.trim() ?? ""; // E.g., $
+                      String numericPart = match.group(2)?.replaceAll(
+                          ",", "") ?? ""; // Remove commas from numbers
+                      // Try parsing the numeric part to double
+                      carrierPrice = double.tryParse(numericPart) ?? 0.0;
+                    }
+                    // Calculate the total price
+                    double price = weight * carrierPrice;
+                    print("Total price: $price");
                     // Handle Pay Later logic here
+                    await apiService.order(
+                      listid: carrier["id"],
+                      weight: weight,
+                      price: price,
+                      currency: carrier["currency"],
+                      notes: "hi",
+                      api:
+                      '/order', // Provide your API base URL
+                    );
+                    AwesomeDialog(
+                      context: context,
+                      dialogType: DialogType.success,
+                      animType: AnimType.topSlide,
+                      title: 'Success',
+                      desc: 'Order Successful',
+                      btnOkIcon: Icons.check,
+                      btnOkOnPress: () {
+                        Get.to(() => const BottomBar());
+                      },
+                    ).show();
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.grey[300], // Pay Later button color
