6036bceb31b053dbb5174eb68f7a5afb7ea2bd51
PravasGiri7 <gpravas4@gmail.com>
2024-11-24 17:44:24 +0900

    test

--- frontend/lib/src/screens/chat/chat_ui.dart
+++ frontend/lib/src/screens/chat/chat_ui.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+
+import '../../api/api_service.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -10,30 +11,26 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreen extends State<ChatScreen> {
-  List<types.Message> _messages = [];
-  final _user = const types.User(
-    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
-  );
+  final ApiService apiService = ApiService();
+  List<Map<String, dynamic>> items = [];  //
+  void initState() {
+    super.initState();
+    fetchListing();
+  }
+  Future<void> fetchListing() async {
+    try {
+      String api = "/order/carrier";
+      List<dynamic> response = await apiService.getListing(api: api) as List;  // Await the response
+      print("Response: $response");
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Chat(messages: _messages, onSendPressed: _handleSendPressed, user: _user)
+      body: Text("haha"),
     );
-  }
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: "id",
-      text: message.text,
-    );
-
-    _addMessage(textMessage);
-  }
-
-  void _addMessage(types.Message message) {
-    setState(() {
-      _messages.insert(0, message);
-    });
   }
 }
+
