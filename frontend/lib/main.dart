1342dedcca53e251f3f5530782e0bb810d31a99c
nicolaics <nicolai.christian.s@gmail.com>
2024-11-26 21:03:56 +0900

    tidy

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jim/firebase_options.dart';
-import 'package:jim/src/screens/listing/add_listing.dart';
 import 'package:jim/src/screens/welcome.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
