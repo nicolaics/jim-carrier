cfe9c710eec5a16133ad391d1e52861e6ef0a654
nicolaics <nicolai.christian.s@gmail.com>
2024-11-26 17:00:21 +0900

    tidy

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,16 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:jim/firebase_options.dart';
 import 'package:jim/src/screens/welcome.dart';
-import 'package:jim/src/screens/home/home_page.dart';
-import 'package:jim/src/screens/order/previous_order.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
