776ec5a701c11c25d0ab715d976d667c0f691e5d
nicolaics <nicolai.christian.s@gmail.com>
2024-11-28 14:51:37 +0900

    add firebase notif class

---
+++ frontend/lib/src/base_class/firebase_notif.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:jim/src/flutter_storage.dart';
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('Title: ${message.notification?.title}');
+  print('Body: ${message.notification?.body}');
+  print('Payload: ${message.data}');
+}
+
+class FirebaseNotification {
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initNotifications() async {
+    await _firebaseMessaging.requestPermission();
+    final fcmToken = await _firebaseMessaging.getToken();
+    print('Token: $fcmToken');
+
+    await StorageService.storeFcmToken(fcmToken);
+
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  }
+}
