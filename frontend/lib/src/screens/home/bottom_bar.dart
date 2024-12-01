01fafbe177a84fac17d7a01aec87e9a004503146
PravasGiri7 <gpravas4@gmail.com>
2024-12-01 16:35:27 +0900

    testing

--- frontend/lib/src/screens/home/bottom_bar.dart
+++ frontend/lib/src/screens/home/bottom_bar.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:jim/src/screens/try.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import '../../api/auth.dart';
+import '../../base_class/login_google.dart';
+import '../../flutter_storage.dart';
+import '../auth/login_screen.dart';
+import '../try.dart';
 import 'home_page.dart';
 import '../order/previous_order.dart';
 import '../received_order/received_order.dart';
 import '../listing/add_listing.dart';
 
 class BottomBar extends StatefulWidget {
-  const BottomBar({Key? key}) : super(key: key);
+  final int initialIndex; // Add a parameter to accept the initial index
+
+  const BottomBar(int i, {Key? key, this.initialIndex = 0}) : super(key: key);
 
   @override
   _BottomBar createState() => _BottomBar();
@@ -15,53 +22,83 @@ class BottomBar extends StatefulWidget {
 
 class _BottomBar extends State<BottomBar> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     final controller = Get.put(NavigationController());
+    controller.setInitialIndex(widget.initialIndex); // Set the initial index
+  }
 
-    return Scaffold(
-      bottomNavigationBar: Obx(
-        () => NavigationBar(
-          elevation: 0,
-          selectedIndex: controller.selectedindex.value,
-          onDestinationSelected: (index) {
-            if (index == 2) {
-              // Navigate to the Add Listing screen separately
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const AddListingScreen()),
-              );
-            } else {
-              // Change the selected index for other screens
-              controller.selectedindex.value = index;
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.find<NavigationController>();
+
+    return WillPopScope(
+      onWillPop: () async {
+        // Show the AwesomeDialog on back press
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.warning,
+          animType: AnimType.scale,
+          title: 'Log Out',
+          desc: 'Are you sure you want to log out?',
+          btnCancelOnPress: () {},
+          btnOkOnPress: () async {
+            // If confirmed, navigate to the login page or close the app
+            logout(api: '/user/logout');
+            StorageService.deleteToken();
+            final controller = Controller();
+            await controller.signOut();
+            if (mounted) {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => const LoginScreen()));
             }
           },
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: "Home"),
-            NavigationDestination(
-                icon: Icon(Icons.document_scanner), label: "Previous"),
-            NavigationDestination(icon: Icon(Icons.add), label: "Add Listing"),
-            NavigationDestination(icon: Icon(Icons.chat), label: "Chat Bot"),
-            NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
-          ],
+        ).show();
+        return false; // Prevent the back button from navigating
+      },
+      child: Scaffold(
+        bottomNavigationBar: Obx(
+              () => NavigationBar(
+            elevation: 0,
+            selectedIndex: controller.selectedindex.value,
+            onDestinationSelected: (index) {
+              if (index == 2) {
+                // Navigate to the Add Listing screen separately
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AddListingScreen()),
+                );
+              } else {
+                // Change the selected index for other screens
+                controller.selectedindex.value = index;
+              }
+            },
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+              NavigationDestination(
+                  icon: Icon(Icons.document_scanner), label: "Previous"),
+              NavigationDestination(icon: Icon(Icons.add), label: "Add Listing"),
+              NavigationDestination(icon: Icon(Icons.chat), label: "Chat Bot"),
+              NavigationDestination(icon: Icon(Icons.person), label: "Profile"),
+            ],
+          ),
         ),
+        body: Obx(() {
+          // Return the screen only for selected indexes other than Add Listing
+          if (controller.selectedindex.value != 2) {
+            return controller.screens[controller.selectedindex.value];
+          }
+          return const SizedBox(); // Empty widget for Add Listing
+        }),
       ),
-      body: Obx(() {
-        // Return the screen only for selected indexes other than Add Listing
-        if (controller.selectedindex.value != 2) {
-          return controller.screens[controller.selectedindex.value];
-        }
-        return const SizedBox(); // Empty widget for Add Listing
-      }),
     );
   }
 }
 
 class NavigationController extends GetxController {
-  // Set the initial index to 0 to show the HomeScreen first
   final Rx<int> selectedindex = 0.obs;
 
-  // Define the list of screens
   final screens = [
     const HomeScreen(),
     const PreviousOrderScreen(),
@@ -69,4 +106,10 @@ class NavigationController extends GetxController {
     const ReceivedOrder(),
     const TryScreen(),
   ];
+
+  void setInitialIndex(int index) {
+    if (index >= 0 && index < screens.length) {
+      selectedindex.value = index;
+    }
+  }
 }
