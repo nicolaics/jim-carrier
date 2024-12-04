97efb7e96d61903bdaecc1c3dc050d49a696643f
PravasGiri7 <gpravas4@gmail.com>
2024-12-04 21:02:33 +0900

    added loading icon

--- lib/src/screens/received_order/received_order.dart
+++ lib/src/screens/received_order/received_order.dart
@@ -13,20 +13,31 @@ class ReceivedOrder extends StatefulWidget {
 }
 
 class _ReceivedOrder extends State<ReceivedOrder> {
-  List<Map<String, dynamic>> items = []; // List to store the updated items
+  List<Map<String, dynamic>> items = [];
   String? selectedValue;
+  bool isLoading = true; // Loading state
+
+  bool _hasFetchedData = false;
 
   @override
   void initState() {
     super.initState();
-    fetchReceivedOrder();
+    if (!_hasFetchedData) {
+      _hasFetchedData = true;
+      fetchReceivedOrder();
+    }
   }
 
-  // Fetch data for the orders
+
   Future<void> fetchReceivedOrder() async {
+    setState(() {
+      isLoading = true; // Show loading spinner
+    });
+
     try {
       String api = "/order/carrier";
       dynamic response = await getReceivedOrders(api: api);
+      print("response $response");
 
       if (response["status"] == "success") {
         List<Map<String, dynamic>> updatedItems = [];
@@ -51,8 +62,7 @@ class _ReceivedOrder extends State<ReceivedOrder> {
             "price": order['price']?.toString() ?? 'N/A',
             "currency": order['currency'] ?? 'MYR',
             "packageImage": imageBytes,
-            "packageLocation": order['packageLocation'] ??
-                'Unknown', // Ensure this is set properly
+            "packageLocation": order['packageLocation'] ?? 'Unknown',
             "orderStatus": order['orderStatus'] ?? 'Unknown',
             "notes": order['notes'] ?? 'Unknown',
           });
@@ -66,119 +76,15 @@ class _ReceivedOrder extends State<ReceivedOrder> {
       }
     } catch (e) {
       print('Error fetching listing: $e');
+    } finally {
+      setState(() {
+        isLoading = false; // Hide loading spinner
+      });
     }
   }
 
   String formatDate(DateTime date) {
     return "${date.year}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}";
-  }
-
-  void _showUpdateLocationModal(
-      BuildContext context, Map<String, dynamic> item) {
-    String? dropdownValue;
-
-    showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
-      ),
-      builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                "Update Location",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-                value: dropdownValue,
-                hint: const Text("Select Status"),
-                items: const [
-                  DropdownMenuItem(
-                    value: "Received by Carrier",
-                    child: Text("Received by Carrier"),
-                  ),
-                  DropdownMenuItem(
-                    value: "In-flight",
-                    child: Text("In-flight"),
-                  ),
-                  DropdownMenuItem(
-                    value: "Arrived at the Destination Country",
-                    child: Text("Arrived at the Destination Country"),
-                  ),
-                  DropdownMenuItem(
-                    value: "With Local Courier",
-                    child: Text("With Local Courier"),
-                  ),
-                  DropdownMenuItem(
-                    value: "Delivered",
-                    child: Text("Delivered"),
-                  ),
-                ],
-                onChanged: (String? value) {
-                  setState(() {
-                    dropdownValue = value;
-                  });
-                },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  if (dropdownValue != null && dropdownValue!.isNotEmpty) {
-                    String orderStatus = "confirmed";
-                    if (dropdownValue == "In-flight" ||
-                        dropdownValue == "Arrived at the Destination Country" ||
-                        dropdownValue == "With Local Courier") {
-                      orderStatus = "en-route";
-                    } else if (dropdownValue == "Delivered") {
-                      orderStatus = "completed";
-                    }
-
-                    // Update location on the server
-                    dynamic response = await updatePackageLocation(
-                      orderNo: int.tryParse(item['id']) ?? 0,
-                      location: dropdownValue,
-                      orderStatus: orderStatus,
-                      api: "/order/package-location",
-                    );
-
-                    if (response["status"] == "success") {
-                      // Find the index of the updated item
-                      int index = items
-                          .indexWhere((element) => element['id'] == item['id']);
-                      if (index != -1) {
-                        // Update the item in the list
-                        setState(() {
-                          items[index]['packageLocation'] =
-                              dropdownValue; // Update the location
-                          items[index]['orderStatus'] =
-                              orderStatus; // Update the order status
-                        });
-                      }
-                    } else {
-                      print("Error updating the location");
-                    }
-
-                    Navigator.pop(context); // Close the modal
-                  } else {
-                    print("No status selected");
-                  }
-                },
-                child: const Text("Update"),
-              ),
-            ],
-          ),
-        );
-      },
-    );
   }
 
   @override
@@ -198,7 +104,21 @@ class _ReceivedOrder extends State<ReceivedOrder> {
         ),
         centerTitle: true,
       ),
-      body: Column(
+      body: isLoading
+          ? const Center(
+        child: CircularProgressIndicator(), // Loading spinner
+      )
+          : items.isEmpty
+          ? const Center(
+        child: Text(
+          "No orders yet",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      )
+          : Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
@@ -208,9 +128,9 @@ class _ReceivedOrder extends State<ReceivedOrder> {
               hint: const Text("SORT BY"),
               items: <String>['Alphabetical', 'High to Low', 'Nearest Date']
                   .map((String value) => DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      ))
+                value: value,
+                child: Text(value),
+              ))
                   .toList(),
               onChanged: (String? newValue) {
                 setState(() {
@@ -226,8 +146,8 @@ class _ReceivedOrder extends State<ReceivedOrder> {
                 final item = items[index];
                 return Card(
                   elevation: 4,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8, horizontal: 16),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12),
                   ),
@@ -255,20 +175,6 @@ class _ReceivedOrder extends State<ReceivedOrder> {
                         Text("Order Status: ${item['orderStatus']}"),
                         Text("Current Location: ${item['packageLocation']}"),
                         Text("Departure Date: ${item['departureDate']}"),
-                        const SizedBox(height: 8),
-                        ElevatedButton(
-                          onPressed: () =>
-                              _showUpdateLocationModal(context, item),
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size.fromHeight(50),
-                            backgroundColor: Colors.blue,
-                          ),
-                          child: const Text(
-                            "Update Location",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ),
                       ],
                     ),
                   ),
@@ -280,4 +186,6 @@ class _ReceivedOrder extends State<ReceivedOrder> {
       ),
     );
   }
+
 }
+
