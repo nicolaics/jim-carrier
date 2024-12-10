3b21e43f7bac26683799c7ec725cb023d819af10
PravasGiri7 <gpravas4@gmail.com>
2024-12-10 21:13:30 +0900

    coming soon page
    import 'package:flutter/cupertino.dart';
    import 'package:flutter/material.dart';
    import 'package:get/get.dart';
    
    class ComingSoonScreen extends StatelessWidget {
      const ComingSoonScreen({super.key});
    
      @override
      Widget build(BuildContext context) {
        // Get the argument passed from the previous screen
        final String title = Get.arguments ?? 'Coming Soon'; // Use a default value if no argument is passed
    
        return Scaffold(
          appBar: AppBar(
            title: Text(title), // Display the dynamic title
          ),
          body: Center(
            child: Text(
              'Coming Soon',
              style: TextStyle(
                fontSize: 24,
                fontWeight: FontWeight.bold,
                color: Colors.blue, // You can change the color as needed
              ),
            ),
          ),
        );
      }quit
    exit
    
    ''

---
+++ lib/src/screens/coming_soon.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ComingSoonScreen extends StatelessWidget {
+  const ComingSoonScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the argument passed from the previous screen
+    final String title = Get.arguments ?? 'Coming Soon'; // Use a default value if no argument is passed
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title), // Display the dynamic title
+      ),
+      body: Center(
+        child: Text(
+          'Coming Soon',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black, // You can change the color as needed
+          ),
+        ),
+      ),
+    );
+  }
+}
