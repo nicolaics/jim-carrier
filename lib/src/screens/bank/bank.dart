d846066514bd65d75f83e1b55118cf31fcaedbd7
nicolaics <nicolai.christian.s@gmail.com>
2024-12-12 15:22:46 +0900

    rm unused import

--- lib/src/screens/bank/bank.dart
+++ lib/src/screens/bank/bank.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jim/src/api/bank_detail.dart';
