9ee3eee958c6abb0e726255c68930607491b8706
nicolaics <nicolai.christian.s@gmail.com>
2024-12-12 15:20:33 +0900

    add delete account api

--- lib/src/api/auth.dart
+++ lib/src/api/auth.dart
@@ -326,3 +326,21 @@ Future<dynamic> updateProfile(
     return writeErrorResponse(response: e.response);
   }
 }
+
+Future<dynamic> deleteAccount() async {
+  try {
+    final response = await dio.delete("/user");
+
+    if (response.statusCode!.isSuccessfulHttpStatusCode) {
+      return writeSuccessResponse(response: response);
+    } else {
+      return writeErrorResponse(response: response);
+    }
+  } on DioException catch (e) {
+    if (e.type == DioExceptionType.connectionTimeout) {
+      return writeConnectionTimeoutResponse();
+    }
+    print('Error occurred: ${e.response?.data['error']}');
+    return writeErrorResponse(response: e.response);
+  }
+}
