42c6149d72e8a4d5a1d5ec5a8769d50462793f55
nicolaics <nicolai.christian.s@gmail.com>
2024-12-12 14:44:55 +0900

    mv getBankDetails to bank_detail.dart

--- lib/src/api/bank_detail.dart
+++ lib/src/api/bank_detail.dart
@@ -46,3 +46,30 @@ Future<dynamic> updateBankDetail(
     return writeErrorResponse(response: e.response);
   }
 }
+
+Future<dynamic> getCarrierBankDetail(
+    {required int carrierID, required api}) async {
+  Map<String, int> body = {
+    'carrierId': carrierID,
+  };
+
+  try {
+    final response = await dio.post(api, data: body);
+
+    if (response.statusCode!.isSuccessfulHttpStatusCode) {
+      if (response.data['status'] == 'exist') {
+        return writeSuccessResponse(response: response);
+      } else {
+        return writeErrorResponse(response: response);
+      }
+    } else {
+      return writeErrorResponse(response: response);
+    }
+  } on DioException catch (e) {
+    if (e.type == DioExceptionType.connectionTimeout) {
+      return writeConnectionTimeoutResponse();
+    }
+    print('Error occurred: ${e.response?.data['error']}');
+    return writeErrorResponse(response: e.response);
+  }
+}
