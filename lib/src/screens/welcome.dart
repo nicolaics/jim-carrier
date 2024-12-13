e59d553307946c1b7d0f2e1664cf22141997d767
nicolaics <nicolai.christian.s@gmail.com>
2024-12-13 16:52:03 +0900

    add loading

--- lib/src/screens/welcome.dart
+++ lib/src/screens/welcome.dart
@@ -17,6 +17,8 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool isLoading = true; // Initially, the loading state is true
+
   @override
   void initState() {
     super.initState();
@@ -25,13 +27,28 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   Future<void> autoLoginProcess() async {
     try {
+      setState(() {
+        isLoading = true; // Start loading before making the API call
+      });
+
       final response = await autoLogin(api: "/user/login/auto");
       if (response["status"] == "success") {
         await StorageService.storeAccessToken(
             response["message"]["access_token"]);
+        setState(() {
+          isLoading = false;
+        });
         Get.to(() => const BottomBar(0));
+      } else {
+        print("HERE ELSE");
+        setState(() {
+          isLoading = false;
+        });
       }
     } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
       print("ERROR HERE: $e");
     }
   }
@@ -44,61 +61,67 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          body: Container(
-            color: Colors.white,
-            padding: const EdgeInsets.all(tDefaultSize),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(AppName,
-                    style: GoogleFonts.lilitaOne(fontSize: 40),
-                    textAlign: TextAlign.center),
-                Image(
-                    image: const AssetImage(WelcomeScreenImage),
-                    height: height * 0.4),
-                Column(
-                  children: [
-                    Text(Welcome,
-                        style: GoogleFonts.anton(fontSize: 35),
-                        textAlign: TextAlign.center),
-                    const SizedBox(height: 10),
-                    Text(WelcomeMessage,
-                        style: GoogleFonts.pacifico(
-                            fontSize: 20, color: Colors.grey),
-                        textAlign: TextAlign.center),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: OutlinedButton(
-                          onPressed: () => Get.to(() => const LoginScreen()),
-                          style: OutlinedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          )),
-                          child: Text(tLogin.toUpperCase(),
-                              style: const TextStyle(
-                                  color: Colors.black, fontSize: 20))),
-                    ),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                      child: ElevatedButton(
-                          onPressed: () => Get.to(() => const RegisterScreen()),
-                          style: OutlinedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              backgroundColor: Colors.black),
-                          child: Text(tSignup.toUpperCase(),
-                              style: const TextStyle(
-                                  color: Colors.white, fontSize: 20))),
-                    ),
-                  ],
+          body: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
                 )
-              ],
-            ),
-          ),
+              : Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(tDefaultSize),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(AppName,
+                          style: GoogleFonts.lilitaOne(fontSize: 40),
+                          textAlign: TextAlign.center),
+                      Image(
+                          image: const AssetImage(WelcomeScreenImage),
+                          height: height * 0.4),
+                      Column(
+                        children: [
+                          Text(Welcome,
+                              style: GoogleFonts.anton(fontSize: 35),
+                              textAlign: TextAlign.center),
+                          const SizedBox(height: 10),
+                          Text(WelcomeMessage,
+                              style: GoogleFonts.pacifico(
+                                  fontSize: 20, color: Colors.grey),
+                              textAlign: TextAlign.center),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: OutlinedButton(
+                                onPressed: () =>
+                                    Get.to(() => const LoginScreen()),
+                                style: OutlinedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                )),
+                                child: Text(tLogin.toUpperCase(),
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 20))),
+                          ),
+                          const SizedBox(width: 10.0),
+                          Expanded(
+                            child: ElevatedButton(
+                                onPressed: () =>
+                                    Get.to(() => const RegisterScreen()),
+                                style: OutlinedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    backgroundColor: Colors.black),
+                                child: Text(tSignup.toUpperCase(),
+                                    style: const TextStyle(
+                                        color: Colors.white, fontSize: 20))),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
         ));
   }
 }
